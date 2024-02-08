afef0f8f88dacbbe0db9f6a08e5cd7c9a88457b5
Carlos Bravo <carlos.bravomer@gmail.com>
2024-02-08 10:33:22 +0100

    navbar + ,main completado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import tunnel from "astro-tunnel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tunnel(), tailwind()],
+});
